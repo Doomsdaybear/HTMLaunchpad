8885f89a30b3ab8c5bef64263402f65032e38bbb
Doomsdaybear <101878029+Doomsdaybear@users.noreply.github.com>
2023-07-26 14:55:38 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+function onekick() {
+    var kickone = new Audio("Assets/Samples/808sample.wav");
+    kickone.play();
+    kickone.currentTime = 0; //recues
+}
+function onesnare() {
+    var snareone = new Audio("Assets/Samples/808snare.wav");
+    snareone.play();
+    snareone.currentTime = 0;
+}
+function oneclap() {
+    var clapone = new Audio("Assets/Samples/808clap.wav");
+    clapone.play();
+    clapone.currentTime = 0;
+}
+function openhihatone() {
+    var hihatone = new Audio("Assets/Samples/808hihatopen.wav");
+    hihatone.play();
+    hihatone.currentTime = 0;
+}
+function closedhihatone() {
+    var hihattwo = new Audio("Assets/Samples/808hihatclosed.wav");
+    hihattwo.play();
+    hihattwo.currentTime = 0;
+}
